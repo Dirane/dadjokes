16b82c6feaa43a46c90e02676e88e38348e88605
Dirane <diranengala@gmail.com>
2022-09-07 23:12:36 +0100

    Initial commit

---
+++ pages/jokes/index.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>jokes</div>
+</template>
+
+<script>
+// import { defineComponent } from '@vue/composition-api'
+
+// export default defineComponent({
+//     setup() {
+        
+//     },
+// })
+export default {}
+</script>
+<style>
+
+</style>
